c59cae60714ee3df13cfc711e58f88eb9e77ff70
Cyb3rX3r0 <37169775+Cyb3rX3r0@users.noreply.github.com>
2018-09-12 22:43:30 +1200

    Created Basic Headings for Weather App
    added basic headings and divs , components for each section of the app

--- weather-application/src/index.js
+++ weather-application/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+//when importing the javascript file you can leave off the js extension
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+//Telling the dom to render out the App component which is the root element in document
+//First thing it takes is the App and the second thing it takes is the location of the component
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
