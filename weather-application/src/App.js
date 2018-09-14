17dc1759031756fde8313217671cc72900d6bf00
Cyb3rX3r0 <37169775+Cyb3rX3r0@users.noreply.github.com>
2018-09-15 11:37:17 +1200

    added simple interface for the Form
    added the Weather Class interface

--- weather-application/src/App.js
+++ weather-application/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Titles from './components/Titles';
 import Form from "./components/Form";
+import Weather from "./components/Weather";
 
 //This involves app class extending the react.componnent class
+
 class App extends Component {
   render() {
 
@@ -19,6 +20,7 @@ class App extends Component {
         
         <Titles />
         <Form />
+        <Weather />
       </div>
     );
   }
