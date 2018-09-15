ba9e4225371449e63c6ab6734ec4add13d784b20
Cyb3rX3r0 <37169775+Cyb3rX3r0@users.noreply.github.com>
2018-09-15 13:10:56 +1200

    Added in Refresh Prevention and Obtains API Object
    obtains API Object

--- weather-application/src/App.js
+++ weather-application/src/App.js
@@ -1,21 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//this webcomponent is the main component which contains all the tiny components
+import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
-class App extends Component {
+const API_KEY = "767ed9a7dc1512091fb873a8e77ca1f4";
+//const API_URL = "http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric";
+
+class App extends React.Component {
+  //render displays the data within the components returns JSX
+  //purpose of the react is to have smaller components which are in one big file which is exported into an html element
+
+  //creating a function to get the weather from the API
+  getWeather = async (e) => {
+
+    e.preventDefault();
+    
+    //the city and the country constants
+    const city = e.target.elements.name.city.value;
+    const country = e.target.elements.name.country.value;
+    
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+
+    //convert the resposne to the API call to a JSON format
+    const data = await api_call.json();
+
+    //log the file
+    console.log(data);
+  }
+
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    return(
+      <div> 
+
+      <Titles />
+      <Form getWeather={this.getWeather}/>
+      <Weather />
+
+            
       </div>
     );
   }
-}
+};
 
 export default App;
+
+
