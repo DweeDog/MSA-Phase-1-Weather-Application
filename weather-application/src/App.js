7d5418b163d8517ee9d2bce12a776e937905619d
Cyb3rX3r0 <37169775+Cyb3rX3r0@users.noreply.github.com>
2018-09-15 12:02:48 +1200

    Created the Basic App since there was Memory Leak Erros
    Used Create - React - app since the pervious app had a memory leak error

--- weather-application/src/App.js
+++ weather-application/src/App.js
@@ -1,30 +1,21 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Titles from './components/Titles';
-import Form from "./components/Form";
-import Weather from "./components/Weather";
-
-//This involves app class extending the react.componnent class
 
 class App extends Component {
   render() {
-
-    //JSX code runs which converts the JSX tag into something javascript can read
     return (
-
-      //has to be within a single div since it can basically only return one parent element
-      //such as one div or paragraph, the paragraph has to be within the div function
-      //purpose of the react is to have multiple components which combine to one file , which is then 
-      //what is imported outside the application
-      <div>
-        
-        <Titles />
-        <Form />
-        <Weather />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
-};
+}
 
-//make this file for the other files to be able to be import.
 export default App;
